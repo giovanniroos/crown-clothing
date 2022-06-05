9074b86357329494739321950a1499afdc57706e
Giovanni Roos <giovanniroos@gmail.com>
2022-06-05 16:39:43 +0200

    Add categories map data in firebase, use in app

--- src/contexts/product.context.jsx
+++ src/contexts/product.context.jsx
@@ -1,13 +1,26 @@
-import { createContext, useState } from "react";
+// import { createContext, useState, useEffect } from "react";
 
-import PRODUCTS from "../shop-data.json";
+// import { getCategoriesAndDocuments } from "../utils/firebase/firebase.utils";
 
-export const ProductContext = createContext({
-    products: []
-});
+// export const ProductContext = createContext({
+//   products: [],
+// });
 
-export const ProductProvider = ({ children }) => {
-  const [products, setProducts] = useState(PRODUCTS);
-  const value = {products};
-  return (<ProductContext.Provider value={value}>{children}</ProductContext.Provider>);
-};
+// export const ProductProvider = ({ children }) => {
+//   const [products, setProducts] = useState([]);
+
+//   useEffect(() => {
+//     async function fetchData() {
+//       const categoryMap = await getCategoriesAndDocuments("categories");
+//       console.log(categoryMap);
+//     }
+//     fetchData();
+//   }, []);
+
+//   const value = { products };
+//   return (
+//     <ProductContext.Provider value={value}>
+//       {children}
+//     </ProductContext.Provider>
+//   );
+// };
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,4 +1,4 @@
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from "firebase/app";
 import {
   getAuth,
   signInWithRedirect,
@@ -8,25 +8,33 @@ import {
   signInWithEmailAndPassword,
   signOut,
   onAuthStateChanged,
-} from 'firebase/auth';
-import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+} from "firebase/auth";
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  getDocs,
+  setDoc,
+  collection,
+  writeBatch,
+  query,
+} from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAbIK3n6JAu5K-PdgTzekg_FP60TZZ8Hfo",
-  authDomain: "crown-clothing-db-33929.firebaseapp.com",
-  projectId: "crown-clothing-db-33929",
-  storageBucket: "crown-clothing-db-33929.appspot.com",
-  messagingSenderId: "40531026286",
-  appId: "1:40531026286:web:7528f89ac9a41206d6617f",
+  apiKey: 'AIzaSyDDU4V-_QV3M8GyhC9SVieRTDM4dbiT0Yk',
+  authDomain: 'crwn-clothing-db-98d4d.firebaseapp.com',
+  projectId: 'crwn-clothing-db-98d4d',
+  storageBucket: 'crwn-clothing-db-98d4d.appspot.com',
+  messagingSenderId: '626766232035',
+  appId: '1:626766232035:web:506621582dab103a4d08d6',
 };
-
 
 const firebaseApp = initializeApp(firebaseConfig);
 
 const googleProvider = new GoogleAuthProvider();
 
 googleProvider.setCustomParameters({
-  prompt: 'select_account',
+  prompt: "select_account",
 });
 
 export const auth = getAuth();
@@ -37,13 +45,43 @@ export const signInWithGoogleRedirect = () =>
 
 export const db = getFirestore();
 
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd
+) => {
+  const batch = writeBatch(db);
+  const collectionRef = collection(db, collectionKey);
+
+  objectsToAdd.forEach((object) => {
+    const docRef = doc(collectionRef, object.title.toLowerCase());
+    batch.set(docRef, object);
+  });
+
+  await batch.commit();
+  console.log("done");
+};
+
+export const getCategoriesAndDocuments = async () => {
+  const collectionRef = collection(db, "categories");
+  const q = query(collectionRef);
+
+  const querySnapshot = await getDocs(q);
+  const categoryMap = querySnapshot.docs.reduce((acc, docSnapshot) => {
+    const { title, items } = docSnapshot.data();
+    acc[title.toLowerCase()] = items;
+    return acc;
+  }, {});
+
+  return categoryMap;
+};
+
 export const createUserDocumentFromAuth = async (
   userAuth,
   additionalInformation = {}
 ) => {
   if (!userAuth) return;
 
-  const userDocRef = doc(db, 'users', userAuth.uid);
+  const userDocRef = doc(db, "users", userAuth.uid);
 
   const userSnapshot = await getDoc(userDocRef);
 
@@ -59,7 +97,7 @@ export const createUserDocumentFromAuth = async (
         ...additionalInformation,
       });
     } catch (error) {
-      console.log('error creating the user', error.message);
+      console.log("error creating the user", error.message);
     }
   }
 
