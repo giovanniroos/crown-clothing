3e7bb68fcec53f91f47490582a6e4d2e37975980
Giovanni Roos <giovanniroos@gmail.com>
2022-06-25 18:22:47 +0200

    Create redux store and add user context

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,7 +1,8 @@
 import { Fragment, useContext } from "react";
 import { Outlet, Link } from "react-router-dom";
+import { useSelector } from 'react-redux';
 
-import { UserContext } from "../../contexts/user.context";
+import { selectCurrentUser } from '../../store/user/user.selector';
 import { CartContext } from "../../contexts/cart.context";
 
 import CartIcon from "../../components/cart-icon/cart-icon.component";
@@ -13,7 +14,7 @@ import { signOutUser } from "../../utils/firebase/firebase.utils";
 import "./navigation.styles.scss";
 
 const Navigation = () => {
-  const { currentUser } = useContext(UserContext);
+  const currentUser = useSelector(selectCurrentUser);
   const { isCartOpen } = useContext(CartContext);
 
   return (
